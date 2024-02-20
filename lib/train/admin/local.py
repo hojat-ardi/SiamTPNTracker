4a1e938e60c8986cacbff2e3fec89971dd6d88d0
hojat-ardi <hojat.rd133@yahoo.com>
2024-02-20 15:25:30 +0100

    directory initialize  for train step

--- lib/train/admin/local.py
+++ lib/train/admin/local.py
@@ -1,13 +1,13 @@
 
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = './results/'    # Base directory for saving network checkpoints.
+        self.workspace_dir = '/home/ardi/Desktop/Results'    # Base directory for saving network checkpoints.
         self.tensorboard_dir = self.workspace_dir + '/tensorboard/'    # Directory for tensorboard files.
-        self.lasot_dir = ''
-        self.got10k_dir = ''
-        self.trackingnet_dir = ''
-        self.coco_dir = ''
-  
+        # self.lasot_dir = ''
+        self.got10k_dir = '/home/ardi/Desktop/Dataset/GOT-10k/got-10k/train'
+        # self.trackingnet_dir = ''
+        # self.coco_dir = ''
+        
 
                 
 
