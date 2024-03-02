56f5d0ec710f8e6f22fe1d101de58b8db58be1d5
hojat-ardi <hojat.rd133@yahoo.com>
2024-03-02 12:51:02 +0100

    change directory for saving network checkpoints

--- lib/train/admin/local.py
+++ lib/train/admin/local.py
@@ -1,7 +1,7 @@
 
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = '/home/ardi/Desktop/Results'    # Base directory for saving network checkpoints.
+        self.workspace_dir = '/home/ardi/Desktop/project/SiamTPNTracker/results'    # Base directory for saving network checkpoints.
         self.tensorboard_dir = self.workspace_dir + '/tensorboard/'    # Directory for tensorboard files.
         # self.lasot_dir = ''
         self.got10k_dir = '/home/ardi/Desktop/Dataset/GOT-10k/got-10k/train'
