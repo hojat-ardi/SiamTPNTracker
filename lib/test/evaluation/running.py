0d52cc9f4a68601e8dc91f91f6a01583f7b9a746
hojat-ardi <hojat.rd133@yahoo.com>
2024-03-30 15:45:03 +0100

    add code of change fps to time per frame (tpf)

--- lib/test/evaluation/running.py
+++ lib/test/evaluation/running.py
@@ -29,16 +29,16 @@ def _save_tracker_output(seq: Sequence, tracker: Tracker, output: dict):
         # print(tracked_bb)
         np.savetxt(file, tracked_bb, delimiter=',', fmt='%.2f')
 
-    # def save_time(file, data):
-    #     exec_times = np.array(data).astype(float)
-    #     np.savetxt(file, exec_times, delimiter='\t', fmt='%f')
+    def save_time(file, data):
+        exec_times = np.array(data).astype(float)
+        np.savetxt(file, exec_times, delimiter='\t', fmt='%f')
 
-    def save_time(file, fps_data):
-    # Convert fps data to frame durations
-    # The duration of a frame is 1/FPS, but for the first frame, it's considered as 0
-        frame_durations = [0] + [1.0 / f for f in fps_data[1:]]  # Skip the first frame for calculation
-        exec_times = np.array(frame_durations).astype(float)
-        np.savetxt(file, exec_times, delimiter='\t', fmt='%.5f')  # Save with three decimal places for precision
+    # def save_time(file, fps_data):
+    # # Convert fps data to frame durations
+    # # The duration of a frame is 1/FPS, but for the first frame, it's considered as 0
+    #     frame_durations = [0] + [1.0 / f for f in fps_data[1:]]  # Skip the first frame for calculation
+    #     exec_times = np.array(frame_durations).astype(float)
+    #     np.savetxt(file, exec_times, delimiter='\t', fmt='%.5f')  # Save with three decimal places for precision
 
     def save_score(file, data):
         scores = np.array(data).astype(float)
